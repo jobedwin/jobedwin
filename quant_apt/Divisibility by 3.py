61b0951c5e1fbdc04375b48f8bb5883496da5233
Jobedwin <jobedwin2020@gmail.com>
2021-11-02 19:11:22 +0530

    intial release

---
+++ quant_apt/Divisibility by 3.py
@@ -0,0 +1,6 @@
+#  A number is divisible by 3 only when the sum of its digits is divisible by 3
+x=int(input("Enter your number:"))
+if(num%3==0):
+    print("{} is divisible by 3".format(num))
+else:
+    print("{} is not divisible by 3".format(num))
