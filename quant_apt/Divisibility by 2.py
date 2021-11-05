4890fa2ad9aab5c70a85cd8c2d371c7fda1207b2
Jobedwin <jobedwin2020@gmail.com>
2021-11-05 19:16:51 +0530

    intial release

--- quant_apt/Divisibility by 2.py
+++ quant_apt/Divisibility by 2.py
@@ -1,6 +1,9 @@
 #  A number is divisible by 2 if its unit digit is any of 0, 2, 4, 6, 8
-x=int(input("Enter your number"))
-if (x%2 ==0):
-    print ("is divisible by 2")
+num=int(input("Enter your number :"))
+x=[int (a) for a in str(num)]
+y = str(x[-1])
+print ("unit digit is", y)
+if (int(y) % 2 ==0):
+    print("is divisible by 2")
 else :
     print ("is not divisible by 2")
