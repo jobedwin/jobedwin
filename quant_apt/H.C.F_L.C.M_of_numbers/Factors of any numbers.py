109b29b1cfe06da50fe291eb48dfcd820e14badc
Jobedwin <jobedwin2020@gmail.com>
2021-12-07 18:00:15 +0530

    intial release

---
+++ quant_apt/H.C.F_L.C.M_of_numbers/Factors of any numbers.py
@@ -0,0 +1,10 @@
+#  Factors of any numbers
+
+num=int(input("Enter your number:"))
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+           
+print_factors(num)
