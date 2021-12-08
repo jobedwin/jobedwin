429b8aaca0cfc060bd12b19d636e51eb57b1fd87
Jobedwin <jobedwin2020@gmail.com>
2021-12-08 19:42:43 +0530

    intial release

---
+++ quant_apt/H.C.F_L.C.M_of_numbers/Multiples of any numbers.py
@@ -0,0 +1,8 @@
+#  Multiples of any number
+
+number = int(input("Enter Your Number: "))
+x=int(input("Which number do you want to multiple?:"))
+print("The multiples are:")
+for i in range (1, x+1):
+    print (number *i)
+
