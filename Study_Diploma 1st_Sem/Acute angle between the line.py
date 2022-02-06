8991a9c0673c66bd47b1d8c3387916cc79540ef0
Jobedwin <jobedwin2020@gmail.com>
2022-02-06 18:20:07 +0530

    intial release

---
+++ Study_Diploma 1st_Sem/Acute angle between the line.py
@@ -0,0 +1,17 @@
+#  To find the acute angle between the lines
+
+
+a= int(input("Enter the value of m1 a : "))
+b= int(input("Enter the value of m1 b : "))
+c= int(input("Enter the value of m1 c : "))
+
+a1= int(input("Enter the value of m2 a : "))
+b1= int(input("Enter the value of m2 b : "))
+c1= int(input("Enter the value of m2 c : "))
+
+m1= -a/b
+m2= -a1/b1
+
+Tan= m1-m2/1 + m1*m2	
+
+print(Tan)
---
+++ Study_Diploma 1st_Sem/Slope of x & y intercept of the line.py
@@ -0,0 +1,12 @@
+#  Slope of x and y intercept of the line
+
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+c = int(input("Enter the value of c : "))
+
+
+s= -a/b
+x= -c/a
+y= -c/b
+
+print(s,x,y)
