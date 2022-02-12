cc30cbcee0b8126ca07563d4275478453113ed65
Jobedwin <jobedwin2020@gmail.com>
2022-02-12 19:09:48 +0530

    intial release

---
+++ Study_Diploma 1st_Sem/Numercial_values_of_allied_angles.py
@@ -0,0 +1,34 @@
+#  Numerical values of allied angles
+
+sin= int(input("Enter the angle for sin : "))
+
+a= 180
+s= (a-sin)
+import math
+print (math.sin(s))
+
+
+cos= int(input("Enter the angle for cos : "))
+
+a= 180
+c= (a-cos)
+print (math.cos(c))
+
+
+tan= int(input("Enter the angle for tan : "))
+
+a= 180
+t= (a-tan)
+print (math.tan(t))
+
+tan= int(input("Enter the angle for tan : "))
+
+a= 360
+t= (a-(-tan))
+print (math.tan(t))
+
+
+cos= int(input("Enter the angle for cos : "))
+
+c= cos
+print (math.cos(c))
