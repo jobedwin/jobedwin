ca635d60c9a90cd5add7ad30f8a81a8920ca788e
Jobedwin <jobedwin2020@gmail.com>
2022-03-16 18:15:51 +0530

    intial release

---
+++ Study_Diploma 1st_Sem/Successive_differentiation.py
@@ -0,0 +1,15 @@
+#  SUccessive differentiation
+from re import X
+
+
+n = int(input(" Enter you number : "))
+p = int(input(" Enter the value of power : "))
+
+c = X
+
+print (n*p)
+
+n1 = int(input(" Enter you number : "))
+p1 = int(input(" Enter the value of power : "))
+
+print (n1*p1)
