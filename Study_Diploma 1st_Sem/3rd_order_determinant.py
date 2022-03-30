cf9687f2d2bd0506777afe865d94944228a4d8e1
Jobedwin <jobedwin2020@gmail.com>
2022-03-30 15:36:16 +0530

    intial release

---
+++ Study_Diploma 1st_Sem/3rd_order_determinant.py
@@ -0,0 +1,23 @@
+#  3rd order determinant
+
+a1=int(input("Enter the number"))
+a2=int(input("Enter the number"))
+a3=int(input("Enter the number"))
+a4=int(input("Enter the number"))
+a5=int(input("Enter the number"))
+a6=int(input("Enter the number"))
+a7=int(input("Enter the number"))
+a8=int(input("Enter the number"))
+a9=int(input("Enter the number"))
+
+A = a1*(a5*a9-a6*a8)
+
+print (A)
+
+A1 = a2*(a4*a9-a6*a7)
+
+print (A1)
+
+A2 = a3*(a4*a8-a5*a7)
+
+print (A2)
---
+++ Study_Diploma 1st_Sem/Inverse_of_square_matrix.py
@@ -0,0 +1,13 @@
+#  Inverse of a square matrix
+
+a1= int(input("Cofactore of : "))
+a2= int(input("Cofactore of : "))
+a3= int(input("Cofactore of : "))
+a4= int(input("Cofactore of : "))
+
+c1= (+(a4))
+c2= (-(a2))
+c3= (-(a3))
+c4= (+(a1))
+
+print (c1,c2,c3,c4)
