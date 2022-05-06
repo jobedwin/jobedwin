75d4267795ebec771e7e8352fc9550cb1414c60a
Jobedwin <jobedwin2020@gmail.com>
2022-05-06 17:40:43 +0530

    intial release

---
+++ Study_Diploma_2nd_Sem/Using_equation_of_concentric_circle.py
@@ -0,0 +1,11 @@
+#  Fiend eUsing equation of the concentric circle 
+
+x= int(input("Enter the value of x :"))
+y= int(input("Enter the value of y :"))
+
+g= int(input("Enter the number of g :"))
+f= int(input("Enter the number of f :"))
+
+k= ((x*x) + (y*y) + g*(x) + f*(y))
+
+print(k)
